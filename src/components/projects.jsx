30a52007ba4d9a872d24c3b4dbee239d94efdf58
SUDO-j182 <jai.onions97@gmail.com>
2025-11-26 04:56:51 +0000

    Created Arrays for individual project cards to be made later

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,8 +1,33 @@
+const projects = [
+    {
+        title: "Password Generator",
+        description: "A tool that generates secure random passwords based on user preferences.",
+        tech: ["Javascript", "HTML", "CSS"],
+        link: "#"
+    },
+    {
+        title: "Hash Comparisom Tool",
+        description: "Checks if two hashed values match",
+        tech: ["Javascript", "HTML", "CSS"],
+        link: "#"
+    }
+];
+
+
 function Projects () {
     return (
          <section id="projects">
           <h2>Projects</h2>
-          <p>PROJECT CARDS GO HERE LATER</p>
+
+          <div className="projects-grid">
+            {projects.map((project) => (
+            <article key={project.title}>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <p><strong>Tech:</strong> {project.tech.join(", ")}</p>
+            </article>
+  ))}
+</div>
         </section>
     );
 }
